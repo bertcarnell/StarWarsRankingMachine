f71831ae116a5a4ff59d5c0bb25480970d4b294c
bertcarnell <bertcarnell@gmail.com>
2018-06-14 19:43:12 -0400

    in progress

--- TrialCodeForFittingModelsToRankings.R
+++ TrialCodeForFittingModelsToRankings.R
@@ -39,11 +39,13 @@ expandRanking <- function(ranks, maxrank)
 {
   m <- max(ranks, na.rm = TRUE) - 1
   assertthat::assert_that(maxrank >= m + 1, msg = "maxrank must be greater than the maximum of ranks")
+  if (maxrank == m + 1) return(ranks)  
   return((ranks - 1)*(maxrank - 1)/m + 1)
 }
 expandRanking(1:4, 5)
 expandRanking(c(1,NA,2,NA,3,NA), 9)
 tryCatch(expandRanking(1:4, 3), error = function(e) "success")
+expandRanking(1:4, 4)
 
 ################################################################################
 # Global Parameters
@@ -75,6 +77,18 @@ X <- apply(X, 1, function(x) {
 # check that the mean rankings were as intended
 cbind(rank(apply(X, 1, mean, na.rm = TRUE)), desiredRanking)
 
+# what if we changed the numeric value of those with NAs
+# 1 2 3 4
+# 1 NA NA 2
+# mean= 1 2 3 3 => bad ranking
+
+# 1 2 3 4
+# 1 NA NA 4
+# mean= 1 2 3 4 => good ranking
+
+Xalt <- apply(X, 2, function(x) expandRanking(x, M))
+cbind(rank(apply(Xalt, 1, mean, na.rm = TRUE)), desiredRanking)
+
 X2 <- rdirichlet(N, desiredRanking)
 X2 <- apply(X2, 1, function(x) {
   # randomly insert NA some times
@@ -94,6 +108,10 @@ X2 <- apply(X2, 1, function(x) {
 
 cbind(rank(apply(X2, 1, mean, na.rm = TRUE)), desiredRanking)
 
+X2alt <- apply(X2, 2, function(x) expandRanking(x, M))
+apply(X2alt, 1, mean, na.rm = TRUE)
+cbind(rank(apply(X2alt, 1, mean, na.rm = TRUE)), desiredRanking)
+
 ###############################################################################
 # create a likelihood function for a vector x of rankings given a 
 #   true set of rankings
@@ -108,7 +126,8 @@ likfunc <- function(theta, x)
     {
       if (is.na(x[i]) || is.na(x[j]))
         next
-      if (x[i] < x[j] && theta[i] < theta[j])
+      if ((x[i] < x[j] && theta[i] < theta[j]) ||
+          (x[i] > x[j] && theta[i] > theta[j]))
       {
         s <- s + 1 / choose(n = length(x), k = 2)
       } 
@@ -151,6 +170,43 @@ generateNewTheta <- function(theta, dat)
 }
 
 # the simulated annealing strategy also does not work
+o <- optim(par = c(1,2,3,6,5,4,9,8,7,10), fn = likfuncmat, gr = generateNewTheta, 
+           dat = X, method = "SANN", control = list(fnscale = -1))
+o$par
+
+likfunc <- function(theta, x)
+{
+  stopifnot(length(x) == length(theta))
+  s <- 0
+  for (i in 1:(length(x) - 1))
+  {
+    for (j in (i + 1):length(x))
+    {
+      if (is.na(x[i]) || is.na(x[j]))
+        next
+      if (x[i] < x[j] && theta[i] < theta[j] ||
+          x[i] > x[j] && theta[i] > theta[j])
+      {
+        s <- s + 1
+      } else
+      {
+        s <- s - 1
+      }
+    }
+  }
+  return(s)
+}
+likfuncmat <- function(theta, dat)
+{
+  s <- 0
+  for (m in 1:ncol(dat))
+  {
+    s <- s + likfunc(theta, dat[,m])
+  }
+  return(s)
+}
+
+# this method works
 o <- optim(par = c(1,2,3,6,5,4,9,8,7,10), fn = likfuncmat, gr = generateNewTheta, 
            dat = X, method = "SANN", control = list(fnscale = -1))
 o$par
@@ -213,7 +269,6 @@ cbind(rank(apply(X2, 1, mean, na.rm = TRUE)), o$par)
 
 ################################################################################
 # Attempt the same model in Stan
-require(rstan)
 
 # use this method if your PC does not have the Rtools directory in its path
 # check
@@ -334,11 +389,11 @@ dat <- list(Npeople = N,
             alpha = alpha,
             stan_na = stan_na);
 fit <- stan(model_code = stanmodelcode, model_name = "example", 
-            data = dat, iter = 2012, chains = 3, cores = 3,
+            data = dat, iter = 2012, chains = 3,
             verbose = TRUE)
 
 print(fit)
-rank(get_posterior_mean(fit)[1:Noptions,4])
+rank(get_posterior_mean(fit)[1:M,4])
 traceplot(fit)
 
 y <- X2
@@ -350,10 +405,195 @@ dat <- list(Npeople = N,
             alpha = alpha,
             stan_na = stan_na);
 fit2 <- stan(model_code = stanmodelcode, model_name = "example", 
-            data = dat, iter = 2012, chains = 3, cores = 3,
+            data = dat, iter = 2012, chains = 3,
             verbose = TRUE)
 
 print(fit2)
-rank(get_posterior_mean(fit2)[1:Noptions,4])
+rank(get_posterior_mean(fit2)[1:M,4])
 traceplot(fit2)
+
+###############################################################################
+
+stanmodelcode2 <- "
+functions {
+real likfunc(vector theta1, vector x, real stan_na)
+{
+  int M = num_elements(x);
+  real s = M * (M - 1.0) / 2.0;
+
+  for (i in 1:(M - 1))
+  {
+    for (j in (i+1):M)
+    {
+      if (fabs(x[i] - stan_na) < 1E-9 || 
+          fabs(x[j] - stan_na) < 1E-9)
+      {
+        s += 0.0;
+      }
+      else if ((x[i] > x[j] && theta1[i] > theta1[j]) ||
+           (x[i] < x[j] && theta1[i] < theta1[j]))
+      {
+        s += 1.0;
+      } 
+      else
+      {
+        s -= 1.0;
+      }
+    }
+  }
+  return s;
+}
+
+real likfuncmat_lpdf(matrix dat, vector theta1, real stan_na)
+{
+  real s = 0;
+  int M = num_elements(theta1);
+  int N = cols(dat);
+  vector[M] person_options;
+  
+  for (n in 1:N)
+  {
+    person_options = dat[:,n];
+    s += likfunc(theta1, person_options, stan_na);
+  }
+  return s;
+}
+}
+data {
+  int<lower=0> Npeople;
+  int<lower=0> Noptions;
+  matrix[Noptions, Npeople] y;
+  vector<lower=0>[Noptions] alpha;
+  real stan_na;
+}
+
+parameters {
+  vector<lower=0, upper=1>[Noptions] theta;
+} 
+
+model {
+  // theta ~ dirichlet(alpha);
+  y ~ likfuncmat(theta, stan_na);
+} 
+"
+
+# stan can't read in missing, so use -9999
+y <- X
+y[which(is.na(X), arr.ind = TRUE)] <- stan_na
+alpha <- c(1,2,3,6,5,4,9,8,7,10)
+dat <- list(Npeople = N, 
+            Noptions = M, 
+            y = y, 
+            alpha = alpha,
+            stan_na = stan_na);
+fit <- stan(model_code = stanmodelcode2, model_name = "example2", 
+            data = dat, iter = 2012, chains = 3, warmup=2000, thin=1,
+            verbose = TRUE)
+
+print(fit)
+rank(get_posterior_mean(fit)[1:M,4])
+traceplot(fit)
+
+y <- X2
+y[which(is.na(X2), arr.ind = TRUE)] <- stan_na
+alpha <- c(1,2,3,6,5,4,9,8,7,10)
+dat <- list(Npeople = N, 
+            Noptions = M, 
+            y = y, 
+            alpha = alpha,
+            stan_na = stan_na);
+fit2 <- stan(model_code = stanmodelcode2, model_name = "example2", 
+             data = dat, iter = 2012, chains = 3,
+             verbose = TRUE)
+
+print(fit2)
+rank(get_posterior_mean(fit2)[1:M,4])
+traceplot(fit2)
+
+###############################################################################
+
+stanmodelcode3 <- "
+functions {
+  real likfunc(vector theta1, vector x, real stan_na)
+  {
+    int M = num_elements(x);
+    real s = M * (M - 1.0) / 2.0;
+    
+    for (i in 1:(M - 1))
+    {
+      for (j in (i+1):M)
+      {
+        if (fabs(x[i] - stan_na) < 1E-9 || 
+            fabs(x[j] - stan_na) < 1E-9)
+        {
+          s += 0.0;
+        }
+        else if ((x[i] > x[j] && theta1[i] > theta1[j]) ||
+                 (x[i] < x[j] && theta1[i] < theta1[j]))
+        {
+          s += 1.0;
+        } 
+        else
+        {
+          s -= 1.0;
+        }
+      }
+    }
+    return s;
+  }
+  
+  real likfuncmat_lpdf(matrix dat, vector theta1, real stan_na)
+  {
+    real s = 0;
+    int M = num_elements(theta1);
+    int N = cols(dat);
+    vector[M] person_options;
+    
+    for (n in 1:N)
+    {
+      person_options = dat[:,n];
+      s += likfunc(theta1, person_options, stan_na);
+    }
+    return s;
+  }
+}
+data {
+  int<lower=0> Npeople;
+  int<lower=0> Noptions;
+  matrix[Noptions, Npeople] y;
+  vector<lower=0>[Noptions] alpha;
+  real stan_na;
+  matrix[Noptions, Noptions] sigma;
+}
+
+parameters {
+  vector<lower=0, upper=1>[Noptions] theta;
+} 
+
+model {
+  theta ~ multi_normal_cholesky(alpha, sigma);
+  y ~ likfuncmat(theta, stan_na);
+} 
+"
+
+# stan can't read in missing, so use -9999
+y <- X
+y[which(is.na(X), arr.ind = TRUE)] <- stan_na
+alpha <- rep(0.5, 10)
+sigma <- matrix(-0.1, M, M)
+diag(sigma) <- 5
+dat <- list(Npeople = N, 
+            Noptions = M, 
+            y = y, 
+            alpha = alpha,
+            stan_na = stan_na,
+            sigma = sigma)
+fit <- stan(model_code = stanmodelcode3, model_name = "example3", 
+            data = dat, iter = 3000, chains = 3, warmup=1000, thin=1,
+            verbose = TRUE)
+
+print(fit)
+rank(get_posterior_mean(fit)[1:M,4])
+traceplot(fit) + ylim(0, 1)
+
 
